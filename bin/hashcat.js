1590ced98ed14940a3ead88894b87aa1b01f960a
mendhak <smaltaf@msn.com>
2013-08-03 23:00:59 +0100

    Usage and argv processing

--- bin/hashcat.js
+++ bin/hashcat.js
@@ -4,9 +4,14 @@ var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 hcat = require(lib + '/libhashcat.js');
 
+if(process.argv.length < 3 || process.argv[2][0] == '-')
+{
+    console.log('Usage: hashcat index.html');
+    process.exit(1);
+}
+
 var options = {
-    htmlFile: 'example/tryMe.html',
-    compress: true
+    htmlFile: process.argv[2]
 }
 
 hcat.hashcatify(options);
