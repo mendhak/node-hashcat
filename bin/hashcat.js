f3160b4cae59867aa92d1e991e722477b0ca282d
mendhak <smaltaf@msn.com>
2013-08-03 06:35:27 +0100

    Corrected lib require path

--- bin/hashcat.js
+++ bin/hashcat.js
@@ -1,5 +1,8 @@
-//Read arguments pass to libhashcat.js
-hcat = require('../lib/libhashcat.js');
+#!/usr/bin/env node
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+hcat = require(lib + '/libhashcat.js');
 
 hcat.process();
 
