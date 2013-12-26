5528238daf893112b6be1d1a429ee8244674826f
mendhak <smaltaf@msn.com>
2013-12-26 15:00:17 +0000

    Made hashcat synchronous since it's a console app

--- bin/hashcat.js
+++ bin/hashcat.js
@@ -2,6 +2,10 @@
 var path = require('path');
 var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+var pjson = require(path.join(path.dirname(fs.realpathSync(__filename)), '../package.json'));
+
+console.log("Hashcat version " + pjson.version);
+
 hcat = require(lib + '/libhashcat.js');
 
 if(process.argv.length < 3 || process.argv[2][0] == '-')
